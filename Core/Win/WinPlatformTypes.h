94f9281e47ce4d5f84e63bbf1e7f146fbd6e2099
yuyang158 <yuyang_158@hotmail.com>
2021-04-17 11:38:22 +0800

    remove all

--- Core/Win/WinPlatformTypes.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "GenericPlatformTypes.h"
-
-struct WinPlatformTypes : public GenericPlatformTypes {
-	typedef unsigned __int64 SIZE_T;
-};
-
-typedef WinPlatformTypes PlatformTypes;
